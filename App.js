f749cb7cb6ccea4c2659d313ae90c2a1e72f304c
Mohau-Comfort <69164161+Mohau-Comfort@users.noreply.github.com>
2023-01-09 13:41:06 +0200

    added new assests and json packages

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { Text, View } from 'react-native';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
+import { Home } from './screens/Home.screen';
+import { Details } from './screens/Details.screen';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View >
-      <Text>Are you still working???</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Details' component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-
-
+export default App;
