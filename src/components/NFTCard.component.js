8db0a6ea9d4a926177952c171be5adca86200d12
Mohau-Comfort <69164161+Mohau-Comfort@users.noreply.github.com>
2023-01-11 13:51:03 +0200

    added nft cards and styled them

--- src/components/NFTCard.component.js
+++ src/components/NFTCard.component.js
@@ -1,9 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, Text } from 'react-native';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { COLORS, SHADOWS, SIZES } from '../utilities';
 
-export const NFTCard = () => {
+
+export const NFTCard = ({ data }) => {
+    const Navigation = useNavigation();
+
     return (
-        <View>
+        <View style={{
+            backgroundColor: COLORS.white,
+            borderRadius: SIZES.font,
+            marginBottom: SIZES.extraLarge,
+            margin: SIZES.base,
+            ...SHADOWS.dark
+        }}>
+            <View style={{ width: "100%", height: 250, }}>
+                <Image
+                    source={data.image}
+                    resizeMode="cover"
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                        borderTopLeftRadius: SIZES.font,
+                        borderTopRightRadius: SIZES.font,
+                    }}
+                />
+            </View>
             <Text>NFTCard</Text>
         </View>
     )
--- src/screens/Home.screen.js
+++ src/screens/Home.screen.js
@@ -3,6 +3,7 @@ import { View, SafeAreaView, Text, FlatList } from "react-native";
 import { COLORS, NFTData } from "../utilities";
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
+
 export const Home = () => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
@@ -10,8 +11,23 @@ export const Home = () => {
             <View style={{ flex: 1 }}>
                 <View style={{ zIndex: 0 }}>
                     <FlatList data={NFTData}
-                        renderItem={({ item }) => <Text>{item.name}</Text>}
+                        renderItem={({ item }) => <NFTCard data={item} />}
+                        keyExtractor={(item) => item.id}
+                        showsVerticalScrollIndicator={false}
+                        ListHeaderComponent={<HomeHeader />}
                     />
+                </View>
+                <View style={{
+                    position: "absolute",
+                    top: 0,
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    zIndex: -1,
+                }}>
+                    <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+                    <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+
                 </View>
             </View>
         </SafeAreaView>
